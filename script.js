68aa213185b0ed27e73b64b52cd95d2308d3c261
alexeyvlasov2021 <78164716+alexeyvlasov2021@users.noreply.github.com>
2024-05-31 02:37:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
+var colors = ['blue', 'yellow', 'red'];
+
+
+function animateColor(color){
+    setInterval(function(){
+    $("button").animate({backgroundColor: color}, "slow");
+}, 1000);
+}
+
+
 
 $(".postcard").hide();
 
-setInterval(function(){
-    $("button").animate({backgroundColor: 'blue'}, "slow");
-}, 1000);
+// setInterval(function(){
+//     $("button").animate({backgroundColor: 'blue'}, "slow");
+// }, 1000);
 
-setInterval(function(){
-    $("button").animate({backgroundColor: 'yellow'}, "slow");
-}, 1000);
+// setInterval(function(){
+//     $("button").animate({backgroundColor: 'yellow'}, "slow");
+// }, 1000);
 
-setInterval(function(){
-    $("button").animate({backgroundColor: 'red'}, "slow");
-}, 1000);
+// setInterval(function(){
+//     $("button").animate({backgroundColor: 'red'}, "slow");
+// }, 1000);
+
+
+
+for(var i=0; i <colors.length; i++){
+    var selectedColor = colors[i];
+    animateColor(selectedColor);
+}
 
 
 
